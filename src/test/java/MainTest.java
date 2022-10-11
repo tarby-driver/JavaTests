34f7d19126a223d6980eb1ca49f1842c60f4bd78
tarby-driver <tarby@mail.ru>
2022-10-11 15:31:47 -0500

    1

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+    @Test
+    void getSumTest(){
+        System.setProperty("webdriver.chrome.driver", "C:/Users/panic/Downloads/chromedriver_win32/chromedriver.exe");
+
+
+    }
+}
